b31a56951b4f0a6a8cb8e252c1ef4c36c733d79d
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2025-08-15 17:12:02 +0300

    starter template

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Inter', sans-serif;
+}
+</style>
